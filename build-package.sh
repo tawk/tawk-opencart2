6e7cf6be9e9133dfa09a58096e894d118a6406cc
Alvin Johnson So <so.alvinjohnson@gmail.com>
2022-04-15 17:56:00 +0800

    added tawk-url-utils lib and used it to match the exclude/include urls

--- build-package.sh
+++ build-package.sh
@@ -15,8 +15,9 @@ mkdir ./upload
 echo "Copying files to upload directory"
 cp -r admin ./upload/
 cp -r catalog ./upload/
+cp -r vendor ./upload/
 
-echo "Creating opencart 3 zip files"
+echo "Creating opencart 2 zip files"
 zip -9 -rq tawk-oc2-$release_version.ocmod.zip upload README.md
 zip -9 -rq tawk-oc2-$release_version.zip admin catalog README.md
 
