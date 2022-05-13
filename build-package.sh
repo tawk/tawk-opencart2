11da7c3ad442824b2db86b25a90e3ac7aaf025f0
Alvin Johnson So <so.alvinjohnson@gmail.com>
2022-05-13 18:01:08 +0800

    updated build script to include new files on non-ocmod package

--- build-package.sh
+++ build-package.sh
@@ -2,24 +2,27 @@
 
 release_version=$(jq -r '.version' ./composer.json);
 
-echo "Creating temporary upload directory"
-rm -rf ./upload
-mkdir ./upload
-mkdir -p ./upload/admin/controller/extension/module/tawkto
-mkdir -p ./upload/catalog/controller/extension/module/tawkto
+echo "Creating temporary directory"
+rm -rf ./tmp/admin;
+rm -rf ./tmp/catalog;
+rm -rf ./tmp/upload;
+mkdir -p ./tmp/upload;
+mkdir -p ./tmp/admin/controller/extension/module/tawkto;
+mkdir -p ./tmp/catalog/controller/extension/module/tawkto;
 
-echo "Copying files to upload directory"
-cp -r admin ./upload/
-cp -r catalog ./upload/
-cp -r ./vendor ./upload/admin/controller/extension/module/tawkto
-cp -r ./vendor ./upload/catalog/controller/extension/module/tawkto
-cp -r ./upgrades ./upload/admin/controller/extension/module/tawkto
+
+echo "Copying files"
+cp README.md ./tmp;
+cp -r admin ./tmp;
+cp -r catalog ./tmp;
+cp -r upgrades ./tmp/admin/controller/extension/module/tawkto;
+cp -r vendor ./tmp/admin/controller/extension/module/tawkto;
+cp -r vendor ./tmp/catalog/controller/extension/module/tawkto;
+cp -r ./tmp/admin ./tmp/upload;
+cp -r ./tmp/catalog ./tmp/upload;
 
 echo "Creating opencart 2 zip files"
-zip -9 -rq tawk-oc2-$release_version.ocmod.zip upload README.md
-zip -9 -rq tawk-oc2-$release_version.zip admin catalog README.md
-
-echo "Cleaning up"
-rm -rf ./upload
+$(cd ./tmp && zip -9 -rq tawk-oc2-$release_version.ocmod.zip upload README.md);
+$(cd ./tmp && zip -9 -rq tawk-oc2-$release_version.zip admin catalog README.md);
 
 echo "Done!"
