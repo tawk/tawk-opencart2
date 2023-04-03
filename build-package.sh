634eca2994a5d4d217c19f89ffa464198ed5add6
Shankar Karuppiah <savadi@gmail.com>
2023-04-03 05:32:37 +0300

    Release/pattern matching (#14)
    * Add tawk-url-utils lib and used it to match the exclude/include urls
    * Add an upgrade manager and handled backwards compatibility for pattern matching
    * Fix version comparison to handle backwards compatibility
    * Updated package structure and added function to check valid hostname instead of directly comparing to the base url
    * Update readme for changelog and new package structure and added example patterns tooltip
    * Added release workflow
    * Updated build script to include new files on non-ocmod package
    * Fix asset path for uploading artifacts
    
    ---------
    
    Co-authored-by: Alvin Johnson So <so.alvinjohnson@gmail.com>
    Co-authored-by: asyraf <asyraf@excelfixer.com>

--- build-package.sh
+++ build-package.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-    then
-        echo "Release version wasn't specified";
-        return;
-fi
+release_version=$(jq -r '.version' ./composer.json);
 
-release_version=$1;
+echo "Creating temporary directory"
+rm -rf ./tmp;
+mkdir -p ./tmp/upload;
+mkdir -p ./tmp/admin/controller/extension/module/tawkto;
+mkdir -p ./tmp/catalog/controller/extension/module/tawkto;
 
-echo "Creating temporary upload directory"
-rm -rf ./upload
-mkdir ./upload
 
-echo "Copying files to upload directory"
-cp -r admin ./upload/
-cp -r catalog ./upload/
+echo "Copying files"
+cp README.md ./tmp;
+cp -r admin ./tmp;
+cp -r catalog ./tmp;
+cp -r upgrades ./tmp/admin/controller/extension/module/tawkto;
+cp -r vendor ./tmp/admin/controller/extension/module/tawkto;
+cp -r vendor ./tmp/catalog/controller/extension/module/tawkto;
+cp -r ./tmp/admin ./tmp/upload;
+cp -r ./tmp/catalog ./tmp/upload;
 
-echo "Creating opencart 3 zip files"
-zip -9 -rq tawk-oc2-$release_version.ocmod.zip upload README.md
-zip -9 -rq tawk-oc2-$release_version.zip admin catalog README.md
-
-echo "Cleaning up"
-rm -rf ./upload
+echo "Creating opencart 2 zip files"
+$(cd ./tmp && zip -9 -rq tawk-oc2-$release_version.ocmod.zip upload README.md);
+$(cd ./tmp && zip -9 -rq tawk-oc2-$release_version.zip admin catalog README.md);
 
 echo "Done!"
